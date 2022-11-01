45f6da404c4fd30c97e67a14bc6648a33ef05a68
Alexander Golubnichi <94615869+AlexGolubnichy@users.noreply.github.com>
2022-11-01 16:54:19 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		}),
+	]
+}
